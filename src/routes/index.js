f55ec87a7139f4d982dba13286236b0b9d6ca731
muckacid <moraga.inf@gmail.com>
2024-04-30 10:15:16 -0400

    se limpia el repositorio para quede de base

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,13 +2,11 @@ const { Router } = require('express')
 const router = Router()
 
 router.get('/', (req, res) => {
-    const title = 'Home'
-    const message = 'hola'
-    res.render('index', { title, message })
+    res.send("mensaje a la web")
 })
 
 router.get('/about', (req, res) => {
-    res.render('about')
+    res.render("mensaje a la web about")
 })
 
 module.exports = router
